733d9896e0f8d0c4e09a96b313af3cd6aeabffc5
haradrime <haradrime@gmail.com>
2017-04-12 16:07:25 +0500

    First commit

---
+++ landing/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+
+def landing(request):
+    name = "Haradrime"
+    current_day = "25.03.2017"
+    form = SubscriberForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+        new_form = form.save()
+
+    return render(request, 'landing/landing.html', locals())
